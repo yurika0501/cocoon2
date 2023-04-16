77c7ec798f8dbe5de9ba4d9ead595727cf9b080d
yurika0501 <titizuzu0501@gmail.com>
2023-04-16 06:51:59 +0000

    [Add]ブートストラップ導入

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,11 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
+import "jquery";
+import "popper.js";
+import "bootstrap";
+import "../stylesheets/application" 
+
 var jQuery = require("jquery");
 global.$ = global.jQuery = jQuery;
 window.$ = window.jQuery = jQuery;
